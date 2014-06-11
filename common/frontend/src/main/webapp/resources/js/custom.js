da27c45bb466027b3d41753d35060f0e48a282ca
robervaldo <robervaldo19@hotmai.com>
2014-06-10 23:31:13 -0300

    versão com AngularJS

--- common/frontend/src/main/webapp/resources/js/custom.js
+++ common/frontend/src/main/webapp/resources/js/custom.js
@@ -4,4 +4,28 @@
 
 function HomeCtrl($scope, $http, $window) {
 	$scope.geral = {dataAtual: '07/06/2014'};
+	$scope.geral = {nome: 'digite um nome'};
 }
+
+var myApp = angular.module('myApp', []);
+
+myApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/Cad', {
+        templateUrl: 'pages/cadastroCliente.html',
+        controller: 'HomeCtrl'
+      }).
+      otherwise({
+    	  redirectTo: '/Cad'
+      });
+  }]);
+
+myApp.controller('CadCtrl', function($scope) {
+	$scope.geral = {nome: 'digite um nome'};
+     
+});
+
+angular.element(document).ready(function(){
+	angular.bootstrap(document, ['myApp']);
+});
