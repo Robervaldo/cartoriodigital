86db940f9b67a09e58826628454a9a48138e29a2
robervaldo <robervaldo19@hotmai.com>
2014-06-12 23:16:00 -0300

    Add Router.js...

--- common/frontend/src/main/webapp/resources/js/custom.js
+++ common/frontend/src/main/webapp/resources/js/custom.js
@@ -2,18 +2,9 @@
  * Cartorio Digital - 07/06/2014
  */
 
-app.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/Cad', {
-        templateUrl: 'resources/pages/cadastroCliente.html',
-        controller: 'BaseController'
-    });
-}]);
-
 app.controller('BaseController', function($scope) {
-    $scope.greeting = 'Welcome!';
-    $scope.geral = {nome: 'Jesus Cristo'};
+    $scope.nome_sistema = 'Notarial';
+    $scope.nome_usuario = ' Nome do Usuário Logado';
 });
 
 
